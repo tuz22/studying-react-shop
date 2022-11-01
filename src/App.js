b3458d157e8bb2714309ab44567409a745ebb6bf
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-02 02:21:45 +0900

    Card 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
-// import mainBanner from './banner.png';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -15,36 +19,36 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      {/* 
-        src에 이미지 파일이 있으면 이렇게 사용 가능
-      <div className='main-banner' style={{ backgroundImage : 'url('+mainBanner+')'}}></div>
-      */}
       <div className='main-banner-bg'>
         <div className='main-banner'></div>
       </div>
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://gcsevmfhfcsn4841109.gcdn.ntruss.com/data/jdsports_data/images/addimg/00/00/22/99/73/b_00218036_add.gif" width="80%"/>
-            <h4>나이키 포스 1 LE</h4>
-            <p>나이키 포스 1 LE는 82년의 하드우드 아이콘을 올 화이트 또는 올 블랙의 데일리 스타일로 재현합니다.</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://image.msscdn.net/images/goods_img/20190723/1101356/1101356_5_500.jpg?t=20211224163140" width="80%"/>
-            <h4>에어 모나치 어글리 화이트</h4>
-            <p>내구성과 쿠셔닝, 착화감으로 아이들의 발을 클래식으로 안내하세요.</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://cdn.shoemarker.co.kr/Upload/ProductImage/020102/30050_1_0500_0500.jpg" width="80%"/>
-            <h4>디파이 올 데이</h4>
-            <p>나이키 맨즈 디파이는 지지력이 높고 쿠션이 안정감있으며 내구성이 뛰어난 가죽 소재의 상판이 발을 고정합니다.</p>
-          </div>
+          {
+            shoes.map((a, i) => {
+              return (
+                <Card shoes={shoes[i]} />
+              )
+            })
+          }
         </div>
       </div>
 
     </div>
   );
+}
+
+function Card(props){
+  let [img] = useState(props.shoes.img );
+
+  return(
+    <div className="col-md-4">
+      <img src={ img } width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+let data = [
+  {
+    id : 0,
+    title : "나이키 포스 1 LE",
+    content : "나이키 포스 1 LE는 82년의 하드우드 아이콘을 올 화이트 또는 올 블랙의 데일리 스타일로 재현합니다.",
+    price : 120000,
+    img : "https://cdn.shoemarker.co.kr/Upload/ProductImage/020102/30974_1_0500_0500.jpg"
+  },
+
+  {
+    id : 1,
+    title : "에어 모나치 어글리 화이트",
+    content : "내구성과 쿠셔닝, 착화감으로 아이들의 발을 클래식으로 안내하세요.",
+    price : 110000,
+    img : "https://image.msscdn.net/images/goods_img/20190723/1101356/1101356_5_500.jpg?t=20211224163140"
+  },
+
+  {
+    id : 2,
+    title : "디파이 올 데이",
+    content : "나이키 맨즈 디파이는 지지력이 높고 쿠션이 안정감있으며 내구성이 뛰어난 가죽 소재의 상판이 발을 고정합니다.",
+    price : 130000,
+    img : "https://cdn.shoemarker.co.kr/Upload/ProductImage/020102/30050_1_0500_0500.jpg"
+  }
+]
+
+export default data;
