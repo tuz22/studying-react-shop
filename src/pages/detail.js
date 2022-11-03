3e83da891010002732546cfb3fc2e440b45e1854
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-03 23:57:12 +0900

    useEffect 조건, clean up function

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -19,23 +19,32 @@ import { useParams } from 'react-router-dom';
 //   }
 // }
 
-
-
 function Detail(props){
-
+  /* 2초 뒤에 사라지는 박스 */
   let [alert, setAlert] = useState(true)
+
   useEffect(() => { // useEffect 안에 있는 코드는 HTML 랜더링 후에 동작
-    // for ( var i = 0; i < 10000; i++ ){
-    //   console.log(i);
-    // }
-    console.log('안녕');
-    setTimeout(() => { setAlert(false) }, 2000);
+    let timerBox = setTimeout(() => { setAlert(false) }, 2000);
+    console.log('리턴문 밖')
+    return () => {
+      console.log('리턴문 안')
+      clearTimeout(timerBox) // clean up function. - mount 될 때 실행x unmount 때 실행
+    }
     
-  }, [])
+  }, []) // <- 조건부분에 [] 이렇게 쓰면 mount 될 때 1회만 실행
 
+  /* 숫자만 입력가능한 input */
+  let [num, setNum] = useState('');
 
-  let [count, setCount] = useState(0)
+  useEffect(() => {
+    if (isNaN(num) == true) {
+      console.log('숫자만!');
+    }
+
+  }, [num])
+
   let {id} = useParams();
+  let [count, setCount] = useState(0)
   let shoesId = props.shoes.find((a) => a.id == id );
 
   return (
@@ -49,6 +58,7 @@ function Detail(props){
       }
       { count }
       <button onClick={() => { setCount(count+1) }}>버튼</button>
+      <input onChange={(e) => { setNum(e.target.value) }} />
       <div className="row">
         <div className="col-md-6">
           <img src={ shoesId.img} width="100%" />
