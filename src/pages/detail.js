955512a22292430a43042ee2c1a19e39fc2601c6
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-03 17:39:51 +0900

    useEffect

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,34 +1,54 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
 
-/* styled-components로 스타일링 */
-let Box = styled.div`
-  background : grey;
-  padding : 20px;
-`
+/* Lifecycle hook 다는 방법 */
+// useEffect(() => {
 
-let BlueBtn = styled.button`
-  background : ${ props => props.bg};
-  color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
-  padding : 10px;
-`
+// })
 
-// 기존 스타일 복사가능
-let NewBtn = styled.button(BlueBtn);
+/* 예전 방법 */
+// class Detail2 extends React.Component {
+//   componentDidMount(){
+
+//   }
+//   componentDidUpdate(){
+
+//   }
+//   componentWillUnmount(){
+
+//   }
+// }
+
+
 
 function Detail(props){
 
-  let {id} = useParams();
-  console.log(id);
+  let [alert, setAlert] = useState(true)
+  useEffect(() => { // useEffect 안에 있는 코드는 HTML 랜더링 후에 동작
+    // for ( var i = 0; i < 10000; i++ ){
+    //   console.log(i);
+    // }
+    console.log('안녕');
+    setTimeout(() => { setAlert(false) }, 2000);
+    
+  }, [])
 
+
+  let [count, setCount] = useState(0)
+  let {id} = useParams();
   let shoesId = props.shoes.find((a) => a.id == id );
 
   return (
     <div className="container">
-      <Box>
-        <BlueBtn bg="blue">버튼</BlueBtn>
-        <BlueBtn bg="orange">버튼</BlueBtn>
-      </Box>
+      {
+        alert == true
+        ? <div className="alert alert-warning">
+            2초 이내 구매시 할인
+          </div>
+        : null
+      }
+      { count }
+      <button onClick={() => { setCount(count+1) }}>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img src={ shoesId.img} width="100%" />
