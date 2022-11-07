5bd1e30e61a9429e208890d6024422ed852e6f61
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-07 23:58:58 +0900

    redux, store에 state 보관

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,13 @@
 import {Table} from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
 function Cart(){
+
+  /* Redux store에서 state 가져옴 */
+  // let a = useSelector((state) => { return state }) // return하는 것만 가져다 씀
+  let a = useSelector((state) => state ) // 중괄호 return은 축약가능
+  console.log(a) // ex) return state.stock이면 a = stock 값만 나옴
+
   return (
     <div>
       <Table>
@@ -8,7 +15,7 @@ function Cart(){
           <tr>
             <th>#</th>
             <th>상품명</th>
-            <th>수량</th>
+            <th>{a.stock}</th> {/* store에서 가져온 값 바인딩 */}
             <th>변경하기</th>
           </tr>
         </thead>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
+/* Redux store에 state 보관하는 방법 */
+let user = createSlice({ // useState 역할
+  name: 'user',
+  initialState : 'Kim'
+}) 
+let stock = createSlice({ // useState 역할
+  name: 'stock',
+  initialState : [10, 11, 12]
+}) 
+
+/* ! Redux store 안에 모두 넣진 말기. 컴포넌트 간 공유가 필요없으면 useState() 사용 */
 export default configureStore({
-  reducer: { }
+  reducer: { 
+    user : user.reducer,
+    stock : stock.reducer
+  }
 }) 
